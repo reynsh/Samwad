d6361780567be4dcd60871714a92efc7a3a8d561
Kapil <krdcoder@gmail.com>
2024-07-23 16:58:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import helper
 import preprocessor
 
 st.sidebar.title("Samwad")
-
+st.set_page_config(page_title="Samwad: WhatsApp Chat Analyser")
 uploaded_file = st.sidebar.file_uploader("Choose a file")
 
 if uploaded_file is not None:
